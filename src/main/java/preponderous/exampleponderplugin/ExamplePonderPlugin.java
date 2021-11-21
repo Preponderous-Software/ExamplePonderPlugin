44f154fd3d5eebb8338577297a86c9a373877f72
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-20 17:03:37 -0700

    initial commit

---
+++ src/main/java/preponderous/exampleponderplugin/ExamplePonderPlugin.java
@@ -0,0 +1,82 @@
+package preponderous.exampleponderplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import preponderous.exampleponderplugin.commands.DefaultCommand;
+import preponderous.exampleponderplugin.commands.HelpCommand;
+import preponderous.ponder.AbstractPonderPlugin;
+import preponderous.ponder.misc.PonderAPI_Integrator;
+import preponderous.ponder.misc.specification.ICommand;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public final class ExamplePonderPlugin extends AbstractPonderPlugin {
+
+    private static ExamplePonderPlugin instance;
+
+    public static ExamplePonderPlugin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        ponderAPI_integrator = new PonderAPI_Integrator(this);
+        toolbox = getPonderAPI().getToolbox();
+        initializeConfigService();
+        initializeConfigFile();
+        registerEventHandlers();
+        initializeCommandService();
+        getPonderAPI().setDebug(false);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length == 0) {
+            DefaultCommand defaultCommand = new DefaultCommand();
+            return defaultCommand.execute(sender);
+        }
+
+        return getPonderAPI().getCommandService().interpretCommand(sender, label, args);
+    }
+
+    private void initializeConfigService() {
+        HashMap<String, Object> configOptions = new HashMap<>();
+        configOptions.put("debugMode", false);
+        getPonderAPI().getConfigService().initialize(configOptions);
+    }
+
+    private void initializeConfigFile() {
+        if (!(new File("./plugins/ExamplePonderPlugin/config.yml").exists())) {
+            getPonderAPI().getConfigService().saveMissingConfigDefaultsIfNotPresent();
+        }
+        else {
+            // pre load compatibility checks
+            if (isVersionMismatched()) {
+                getPonderAPI().getConfigService().saveMissingConfigDefaultsIfNotPresent();
+            }
+            reloadConfig();
+        }
+    }
+
+    private void registerEventHandlers() {
+        /*
+        ArrayList<Listener> listeners = new ArrayList<>();
+        getToolbox().getEventHandlerRegistry().registerEventHandlers(listeners, this);
+        */
+    }
+
+    private void initializeCommandService() {
+        ArrayList<ICommand> commands = new ArrayList<>(Arrays.asList(
+                new HelpCommand()
+        ));
+        getPonderAPI().getCommandService().initialize(commands, "That command wasn't found.");
+    }
+}
---
+++ src/main/java/preponderous/exampleponderplugin/commands/DefaultCommand.java
@@ -0,0 +1,25 @@
+package preponderous.exampleponderplugin.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import preponderous.exampleponderplugin.ExamplePonderPlugin;
+import preponderous.ponder.misc.AbstractCommand;
+
+/**
+ * @author Daniel Stephenson
+ */
+public class DefaultCommand extends AbstractCommand {
+
+    @Override
+    public boolean execute(CommandSender commandSender) {
+        commandSender.sendMessage(ChatColor.AQUA + "ExamplePonderPlugin " + ExamplePonderPlugin.getInstance().getVersion());
+        commandSender.sendMessage(ChatColor.AQUA + "Developed by: Daniel Stephenson");
+        commandSender.sendMessage(ChatColor.AQUA + "Wiki: https://github.com/Preponderous-Software/ExamplePonderPlugin/wiki");
+        return true;
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender, String[] strings) {
+        return execute(commandSender);
+    }
+}
