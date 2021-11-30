6808d0a998e9019c0eece404e2d3664b386e63e2
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-29 22:34:35 -0700

    Added a logger class

---
+++ src/main/java/preponderous/exampleponderplugin/utils/Logger.java
@@ -0,0 +1,26 @@
+package preponderous.exampleponderplugin.utils;
+
+import preponderous.exampleponderplugin.ExamplePonderPlugin;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        if (ExamplePonderPlugin.getInstance().isDebugEnabled()) {
+            System.out.println("[ExamplePonderPlugin] " + message);
+        }
+    }
+
+}
